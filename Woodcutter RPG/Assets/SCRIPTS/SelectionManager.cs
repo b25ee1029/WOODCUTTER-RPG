ae23bfa10743428d1e4a0f47b1125e01318abd21
Qwerty-Coded <b25me1036@iitj.ac.in>
2025-12-01 06:25:53 +0530

    terrain, movement and creature

---
+++ Woodcutter RPG/Assets/SCRIPTS/SelectionManager.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using TMPro;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class SelectionManager : MonoBehaviour
+{
+ 
+    public GameObject interaction_Info_UI;
+    TextMeshProUGUI interaction_text;
+ 
+    private void Start()
+    {
+        interaction_text = interaction_Info_UI.GetComponent<TextMeshProUGUI>();
+    }
+ 
+    void Update()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+        {
+            var selectionTransform = hit.transform;
+ 
+            if (selectionTransform.GetComponent<InteractableObject>())
+            {
+                interaction_text.text = selectionTransform.GetComponent<InteractableObject>().GetItemName();
+                interaction_Info_UI.SetActive(true);
+            }
+            else 
+            { 
+                interaction_Info_UI.SetActive(false);
+            }
+ 
+        }
+    }
+
+}
